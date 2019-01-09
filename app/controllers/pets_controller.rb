b764ab9c8271e5752a4250eeeeee380e20fbbfe7
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-09 21:33:18 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do
     binding.pry
     @pet = Pet.create(params[:pet])
-    if !params["owner"]["name"].empty?
+    if !params["owner_name"]["pet_name"].empty?
       @pet.owners << Owner.create(name: params["owner"]["name"])
     end
     redirect to "pets/#{@pet.id}"
