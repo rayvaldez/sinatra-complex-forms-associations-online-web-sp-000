0c35a9cbab87072df58647c68ce48eafb3660ec5
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-09 22:59:38 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -46,4 +46,9 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end
+
+  get '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/show'
+  end
 end
